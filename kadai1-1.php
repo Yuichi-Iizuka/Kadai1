9c7c763618970bf3092c4b55eb0456ad2507ca5e
Yuichi-Iizuka <tyasu634@gmail.com>
2020-12-12 15:32:01 +0900

    修正しました。よろしくお願い致します。

--- kadai1-1.php
+++ kadai1-1.php
@@ -34,11 +34,11 @@
     }
 
     for ($i = 1; $i < 100; $i++) {
-      if ($i % $fizz == 0 && $i % $buzz == 0) {
+      if ($i % $fizz === 0 && $i % $buzz === 0) {
         echo '<br>' . 'fizzbuzz;' . $i;
-      } elseif ($i % $fizz == 0) {
+      } elseif ($i % $fizz === 0) {
         echo '<br>' . 'fizz;' . $i;
-      } elseif ($i % $buzz == 0) {
+      } elseif ($i % $buzz === 0) {
         echo '<br>' . 'buzz;' . $i;
       }
     }
